c3d57134b3d36c451def478271b5b0c3d5e38770
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-30 00:32:15 -0700

    Add files via upload

--- Remove K Digits.py
+++ Remove K Digits.py
@@ -6,7 +6,8 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         i, N = 0, len(num)
         stack = []
-        
+        if k >= len(num):
+            return "0"
         while i < N:
             while stack and k > 0 and int(stack[-1]) > int(num[i]):
                 stack.pop()
@@ -17,3 +18,16 @@ class Solution:
             stack.pop()
             k -= 1
         return str(int("".join(stack)))
+
+        # Solution 2
+        stack = []
+        if k >= len(num):
+            return "0"
+        for c in num:
+            while stack and k > 0 and stack[-1] > c:
+                k -= 1
+                stack.pop()
+            stack.append(c)
+        stack = stack[:len(stack) - k]
+        res = "".join(stack)
+        return str(int(res))
